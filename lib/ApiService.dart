8b9f817ca7ba63331f3beac6ee1e387fe67177b1
Ashish <ashishaggarwal7772@gmail.com>
2022-04-08 14:53:31 +0530

    update

--- lib/ApiService.dart
+++ lib/ApiService.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class ApiServices{
 
-  String url = "http://192.168.0.103:4324/sign";
-  String url2 = "http://192.168.0.103:4322/api/v1/certificate";
+  String url = "http://192.168.1.35:4324/sign";
+  String url2 = "http://192.168.1.35:4322/api/v1/certificate";
+  String url3 = "http://192.168.1.35:4322/api/v1/unzip/certificate";
+  String url4 = "http://192.168.1.35:4324/verify";
   Future postData(data)async{
     var response = await http.post(Uri.parse(url),body: data);
-
     //print(" ${json.decode(response.body)} ");
     return response.body.toString();
   }
@@ -20,18 +23,27 @@ class ApiServices{
     //print(" ${json.decode(response.body)} ");
     return response2.body.toString();
   }
+  Map<String, String> headers2 = {
+    "Content-Type": "text/html",
+    "Accept": "text/html",
+    "Content-Length": "1009"
+  };
+  Future postResponseForUnzip(data3)async{
+    var response3 = await http.post(Uri.parse(url3),body: data3,headers:headers2);
+   // print(" ${json.decode(response3.body)} ");
+    return response3.body.toString();
+  }
+
+  Map<String, String> headers3 = {
+   // "Content-Type": "application/json",
+    "Content-Length": "1052"
+  };
+  Future postResponseForVerifyData(data3)async{
+
+    var response4 = await http.post(Uri.parse(url4),body: data3,headers:headers3);
+    // print(" ${json.decode(response3.body)} ");
+    return response4.body.toString();
+  }
 
 
-// Future<CertificateResponseModel?> createUser(data) async{
-//   String url = "http://192.168.0.102:4324/sign";
-//   var response = await http.post(Uri.parse(url),body: data);
-//     print(" ${json.decode(response.body)} ");
-//     if(response.statusCode == 201)
-//       {
-//         final String responseString = json.decode(response.body);
-//         return CertificateResponseModel.fromJson();
-//       }else{
-//       return null;
-//     }
-// }
 }
